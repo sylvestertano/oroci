341445673d245c0d658e3060929e949bdee489b0
sylvestertano <sjttano@gmail.com>
2014-06-06 22:48:12 -0500

    second commit message

---
+++ MemberForm.java
@@ -0,0 +1,24 @@
+public class MemberForm {
+	private int memberId  auto_increment;
+	private String firstName;
+	private String midleName;
+	private String lastName;
+	private String homePhone;
+	private String cellPhone;
+	private String address;
+	private String city;
+	private String state;
+	private String zipCode;
+	private String emailAddress;
+	private String sex;
+	private String active;
+	public enum active {
+		true, false
+	}
+	
+	public enum Sex {
+		M, F
+	}
+	
+     }
+	
